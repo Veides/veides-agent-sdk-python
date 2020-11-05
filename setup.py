d105e8d8cd49c37e93dd685300d5f76163074dee
Gracjan Orzechowski <orzechowski.gracjan@gmail.com>
2020-11-05 08:43:47 +0100

    Removed python 3.5 from supported versions

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
